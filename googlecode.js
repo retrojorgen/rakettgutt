cac95ceaa7557f680f9164db11dab547d3211b09
Jørgen Jacobsen <jorgeja@gmail.com>
2016-02-26 14:00:21 +0100

    update

---
+++ googlecode.js
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- article side -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-5717634590071934"
+     data-ad-slot="5003054406"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
