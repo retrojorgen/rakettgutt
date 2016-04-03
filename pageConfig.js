2e5310eddc2b0a925fb440ff0a64fcef3e62ee09
Jørgen Jacobsen <retrojorgen@Jrgens-MacBook-Air.local>
2016-04-03 19:20:57 +0200

    updated rakettgutt

---
+++ pageConfig.js
@@ -0,0 +1,69 @@
+    $(function () {
+      $('figure').each(function () {
+        if($(this).find('img').length) {
+          $(this).css('width', $(this).find('img').width());
+        }
+      });
+
+      if($("#article-identificator").length) {
+        var articleIdentificator = $("#article-identificator");
+        var ids = articleIdentificator.attr('attr-ids').split(",");
+        console.log(ids);
+        if(ids.indexOf("special") > -1) {
+          $(".main-article").addClass("special");
+          $("body").addClass("special-body");
+          
+        }
+
+        if(ids.indexOf("special-color") > -1) {
+          $("body").css("background-color", $("main").css("background-color"));
+        }
+
+        if(ids.indexOf("special-scroll") > -1) {
+          $(".main-article").addClass("special-scroll");
+          var layers = [];
+          $(".layer").each(function () {
+            layers.push({
+              element: $(this),
+              offset: $(this).offset().top,
+              top: $(this).offset().top + $(this).outerHeight(true),
+              visible: false
+            });
+          });
+
+
+
+          $(window).on('scroll', function () {
+            layers.forEach(function (element, index) {
+              
+              if(index == 0) {
+                console.log(window.scrollY, element.offset, element.top, element.visible, (window.scrollY >= element.offset), ( window.scrollY <= element.top));
+              }
+              if(window.scrollY >= element.offset && window.scrollY <= element.top && !element.visible) {
+                element.element.find('.overlay').addClass('visible');
+                element.visible = true;
+              }
+              if(window.scrollY < element.offset || window.scrollY > element.top && element.visible) {
+                element.element.find('.overlay').removeClass('visible');
+                element.visible = false;
+              }
+            });
+          });
+
+        }
+      }
+
+
+      // fix ratio youtube
+      $('iframe').each(function () {
+        if($(this).attr("src").indexOf("youtube") > -1) {
+          var currentHeight = $(this).attr("height");
+          var currentWidth = $(this).attr("width");
+          var newWidth = $(".articles-wrapper").width();
+          var newHeight = (currentHeight / currentWidth) * newWidth;
+
+          $(this).attr("height", newHeight);
+          $(this).attr("width", newWidth);
+        }
+      });
+    });
