781d7060a9a4fbe5990ed773ace071fdf3612803
Jørgen Jacobsen <jorgeja@gmail.com>
2016-04-18 15:34:09 +0200

    updated

--- frontScript.js
+++ frontScript.js
@@ -2,11 +2,20 @@ $(function () {
 	console.log('hello');
 	var mousePosX = window.innerWidth / 2;
 	var mousePosY = window.innerHeight / 2;
+	
+
+	var resetTimeout = function () {
+		console.log("resetting");
+		TweenMax.to("#rakett-gutt", 0.25, {x: 0, y: 0, rotation: "0deg", ease: Power4.easeOut});
+		TweenMax.to("#rakett-gutt-small", 0.25, {x: 0, y: 0, rotation: "0deg", ease: Power4.easeOut});
+		TweenMax.to("#rakett-gutt-smaller", 0.25, {x: 0, y: 0, rotation: "0deg", ease: Power4.easeOut});
+	};
+
+	var timeout = setTimeout(resetTimeout, 1000);
 	$(document).on("mousemove", function (event) {
 		
 		if(mousePosX == undefined) mousePosX = event.pageX;
 		if(mousePosY == undefined) mousePosY = event.pageY;
-		console.log(event.pageX, event.pageY, mousePosX, mousePosY);
 		var movePosX = (mousePosX - event.pageX) / 15;
 		var movePosY = (mousePosY - event.pageY) / 15;
 		$("#rakett-gutt").css('transform', 'translate(' + movePosX + 'px, ' + movePosY + 'px) rotate(' + movePosX/8 + 'deg)');
